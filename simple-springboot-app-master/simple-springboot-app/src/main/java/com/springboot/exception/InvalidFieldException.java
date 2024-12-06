0e6bcf99c61c1e7abd281d7363e8f62fcb21b34b
NSODHI-C <nsodhi-c@txdot.gov>
2024-12-06 13:53:01 -0600

    AB#51 : Without GitHub Copilot: Java 17 Upgrade - Update Java language features

--- simple-springboot-app-master/simple-springboot-app/src/main/java/com/springboot/exception/InvalidFieldException.java
+++ simple-springboot-app-master/simple-springboot-app/src/main/java/com/springboot/exception/InvalidFieldException.java
@@ -2,19 +2,10 @@ package com.springboot.exception;
 
 public class InvalidFieldException extends RuntimeException {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private String message;
+    public InvalidFieldException(String message) {
+        super(message);
+    }
 
-	public InvalidFieldException(String message) {
-		this.setMessage(message);
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
 }
