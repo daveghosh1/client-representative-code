26031324a490a60be04b1220e2fe350981da217e
RVADDI-C <RVADDI-C@txdot.gov>
2024-12-09 13:30:00 -0600

    "AB#45- Update Java language features"

--- simple-springboot-app-master/simple-springboot-app/src/main/java/com/springboot/exception/InvalidFieldException.java
+++ simple-springboot-app-master/simple-springboot-app/src/main/java/com/springboot/exception/InvalidFieldException.java
@@ -1,6 +1,6 @@
 package com.springboot.exception;
 
-public class InvalidFieldException extends RuntimeException {
+public final class InvalidFieldException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
