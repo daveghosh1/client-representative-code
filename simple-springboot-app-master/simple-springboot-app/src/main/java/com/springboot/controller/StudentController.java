690b4d9ae76120fc287fd9e122f54ea85e2f5f38
NGOWRI-C <ngowri-c@txdot.gov>
2024-12-12 12:52:50 -0600

    AB#53  AB#77 AB#84 AB#92 AB#108 AB#100 : Naga 2 - Without GitHub Copilot:

--- simple-springboot-app-master/simple-springboot-app/src/main/java/com/springboot/controller/StudentController.java
+++ simple-springboot-app-master/simple-springboot-app/src/main/java/com/springboot/controller/StudentController.java
@@ -24,12 +24,22 @@ public class StudentController {
 		return String.format("Authorization %s is valid, and Data is saved", authorization);
 	}
 
+	/**
+	 * Get api call for fetching list of students
+	 *
+	 * @return List of Students
+	 */
 	@GetMapping(value = "/students")
 	public List<Student> getAllHistory() {
 		List<Student> allStudentHistory = studentService.getAllStudentHistory();
 		return allStudentHistory;
 	}
 
+	/**
+	 * Get api call for fetching student data based on student name
+	 *
+	 * @return Student info
+	 */
 	@GetMapping(value = "/{studentName}")
 	public Student getStudentByName(@PathVariable String studentName) {
 		Student studentDetail = studentService.getStudentDetailsByName(studentName);
