e5df5f650c8bd0e2a5f6b5a7849b47b5e78623ca
linakar <LPERUM-C@txdot.gov>
2025-01-10 14:35:29 -0600

    "AB#182-Update Java Language features"

--- simple-springboot-app-master/simple-springboot-app/src/main/java/com/springboot/service/Cat.java
+++ simple-springboot-app-master/simple-springboot-app/src/main/java/com/springboot/service/Cat.java
@@ -3,7 +3,7 @@ package com.springboot.service;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
-//@Primary
+@Primary
 @Service
 public class Cat implements Animal {
 
